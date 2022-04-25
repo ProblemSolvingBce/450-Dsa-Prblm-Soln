0f7649d9db510e041d7db8f0e03d043b3ff3bb6b
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2022-04-25 19:53:23 +0530

    Create set_code.cpp

---
+++ Koushik Biswas/Array/Intersection of Two Arrays/set_code.cpp
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/intersection-of-two-arrays/
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>a_set,b_set;
+        vector<int>v;
+        for(auto it:nums1)
+            a_set.insert(it);
+        for(auto it:nums2)
+            b_set.insert(it);
+        
+        if(a_set.size()>=b_set.size()){
+            for(auto it:a_set){
+                if(b_set.find(it)!=b_set.end())
+                    v.emplace_back(it);
+            }
+        }
+        else{
+            for(auto it:b_set){
+                if(a_set.find(it)!=a_set.end())
+                    v.emplace_back(it);
+            }
+        }
+        return v;
+    }
+};
